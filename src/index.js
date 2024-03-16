177be10c9e3d30661b83a8967275494173c8f9ef
kwicker22 <kwicker2021@gmail.com>
2024-03-15 20:12:05 -0500

    solution 1-4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                                            //Solution 1//                       
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Function/>
+//   </React.StrictMode>
+// );
+
+
+
+
+// function Function(){
+  
+//   return <div className = "backGround">
+//     <h1 >Hello World</h1>
+//     </div>
+// }
+
+
+                                           //Solution 2//
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Button/>
+//   </React.StrictMode>
+// );
+
+// function Button(){
+//   return (
+//     <div>
+//       <button onClick={()=>
+//       alert('Clicked!')}>CLICK ME!</button>
+//     </div>
+//   )
+// }
+
+                                        //Solution 3//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Button>Clicked</Button>
+//     <Button>Clicked1</Button>
+//     <Button>Clicked2</Button>
+//   </React.StrictMode>
+// );
+
+// function Button(props){
+// return (
+//   <div>
+//     <button onClick={()=>
+//     alert(props.children)}>CLICK ME!</button>
+//   </div>
+// )
+// }
+
+
+                                       //Solution 4//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <ClickButton/>
+//   </React.StrictMode>
+// );
+
+
+// function ClickButton(){
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <p> You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   )
+// }
